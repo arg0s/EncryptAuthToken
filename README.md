b2ded0dcf744cc7d57e5f5d57473a68bb2b8cbe3
Aravind Krishnaswamy <Aravind Krishnaswamy>
2014-09-10 01:37:36 +0530

    py3

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ EncryptAuthToken
 ================
 
 A simple module to help you encrypt/decrypt social media authentication tokens with AES before storing them... well, wherever you'd like to!
+
+<pre>
--- crypt.py
+++ crypt.py
@@ -9,7 +9,7 @@ BLOCK_SIZE = 32
 # the character used for padding--with a block cipher such as AES, the value
 # you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
 # used to ensure that your value is always a multiple of BLOCK_SIZE
-PADDING = '{'
+PADDING = b'{'
 
 # one-liner to sufficiently pad the text to be encrypted
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
@@ -21,9 +21,9 @@ DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
 
 def encrypt_token(secret, token):
     cipher = AES.new(secret[:BLOCK_SIZE])
-    return EncodeAES(cipher, token)
+    return EncodeAES(cipher, token.encode('utf-8'))
 
 
 def decrypt_token(secret, code):
     cipher = AES.new(secret[:BLOCK_SIZE])
-    return DecodeAES(cipher, code)
+    return DecodeAES(cipher, code).decode('utf-8')
